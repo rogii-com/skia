3e9ceeed3852060c749c7c1ef2a9d7b55ed3cf06
Alexander Shkarlatov <crazyalex25@mail.ru>
2022-11-11 15:53:17 +0400

    Change flags

--- rogii/run_build.bat
+++ rogii/run_build.bat
@@ -7,8 +7,8 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 python bin\fetch-gn
 python tools\git-sync-deps
 
-bin\gn gen out\release --args="is_component_build=true is_debug=false"
+bin\gn gen out\release --args="is_official_build=true is_component_build=true is_debug=false skia_use_fontconfig=false skia_use_freetype=false skia_enable_tools=false  skia_use_system_libjpeg_turbo=false  skia_use_system_libwebp=false  skia_use_system_libpng=false  skia_use_system_icu=false  skia_use_system_harfbuzz=false"
 ninja -C out\release skia
-bin\gn gen out\debug --args="is_component_build=true is_debug=true"
+bin\gn gen out\debug --args="is_official_build=false is_component_build=true is_debug=true skia_use_fontconfig=false skia_use_freetype=false skia_enable_tools=false  skia_use_system_libjpeg_turbo=false  skia_use_system_libwebp=false  skia_use_system_libpng=false  skia_use_system_icu=false  skia_use_system_harfbuzz=false"
 ninja -C out\debug skia
 
