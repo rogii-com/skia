468b880a665a911a5e468b9fd08dcf0006e3ed63
Alexander Shkarlatov <crazyalex25@mail.ru>
2022-11-11 16:40:18 +0400

    is_official_build=false

--- rogii/run_build.sh
+++ rogii/run_build.sh
@@ -7,7 +7,7 @@ export PATH="${PWD}/depot_tools:${PATH}"
 python bin/fetch-gn
 python tools/git-sync-deps
 
-bin/gn gen out/release --args="is_official_build=true is_component_build=true is_debug=false skia_use_fontconfig=false skia_use_freetype=false skia_enable_tools=false  skia_use_system_libjpeg_turbo=false  skia_use_system_libwebp=false  skia_use_system_libpng=false  skia_use_system_icu=false  skia_use_system_harfbuzz=false"
+bin/gn gen out/release --args="is_official_build=false is_component_build=true is_debug=false skia_use_fontconfig=false skia_use_freetype=false skia_enable_tools=false  skia_use_system_libjpeg_turbo=false  skia_use_system_libwebp=false  skia_use_system_libpng=false  skia_use_system_icu=false  skia_use_system_harfbuzz=false"
 ninja -C out/release skia
 bin/gn gen out/debug --args="is_official_build=false is_component_build=true is_debug=true skia_use_fontconfig=false skia_use_freetype=false skia_enable_tools=false  skia_use_system_libjpeg_turbo=false  skia_use_system_libwebp=false  skia_use_system_libpng=false  skia_use_system_icu=false  skia_use_system_harfbuzz=false"
 ninja -C out/debug skia
