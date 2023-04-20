f1b449ad7e4ad101498018f83f7e99bae3273e4e
Alexander Shkarlatov <crazyalex25@mail.ru>
2023-04-20 16:40:17 +0400

    Use python3

--- rogii/run_build.sh
+++ rogii/run_build.sh
@@ -4,8 +4,8 @@ export PATH="${PWD}/depot_tools:${PATH}"
 
 
 ./tools/install_dependencies.sh --yes
-python bin/fetch-gn
-python tools/git-sync-deps
+python3 bin/fetch-gn
+python3 tools/git-sync-deps
 
 bin/gn gen out/release --args="is_official_build=false is_component_build=true is_debug=false skia_use_fontconfig=false skia_use_freetype=false skia_enable_tools=false  skia_use_system_libjpeg_turbo=false  skia_use_system_libwebp=false  skia_use_system_libpng=false  skia_use_system_icu=false  skia_use_system_harfbuzz=false"
 ninja -C out/release skia
